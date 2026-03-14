fffbb911ea5e17171da512fa6ed92b25f0389d67
JosepER <jespasareig@gmail.com>
2026-03-14 16:36:22 +0100

    end courseinfo

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,26 +1,19 @@
 import { useState } from 'react'
 
-const Statistics = (props) => {
-
-  if (props.good === 0 && props.neutral === 0 && props.bad === 0) {
-    return (
-      <div>
-        <p>No feedback given</p>
-      </div>
-    )
-  }
-
-  return(
-    <div>
-        <p>good {props.good}</p>
-        <p>neutral {props.neutral}</p>
-        <p>bad {props.bad}</p>
-        <p>average {props.average}</p>
-        <p>positive {props.positive}</p>
-    </div>
+const Button = (props) => {
+  
+  return (
+    <button onClick={props.handleClick}>{props.text}</button>
   )
-
 }
+
+
+const StatisticLine = (props) => (
+  <tr>
+    <td>{props.text}</td>
+    <td>{props.value}</td>
+  </tr>
+)
 
 const App = () => {
   // save clicks of each button to its own state
@@ -35,12 +28,27 @@ const App = () => {
   return (
     <div>
       <h1>give feedback</h1>
-        <button onClick={() => setGood(good + 1)}>good</button>
-        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-        <button onClick={() => setBad(bad + 1)}>bad</button>
+        <Button handleClick={() => setGood(good + 1)} text="good" />
+        <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+        <Button handleClick={() => setBad(bad + 1)} text="bad" />
       <h1>statistics</h1>
-      <Statistics good={good} neutral={neutral} bad={bad} average={average} positive={positive} />
-        
+      {good === 0 && neutral === 0 && bad === 0 ? (
+        <p>No feedback given</p>
+      ) : (
+        <table>
+          <tbody>
+            {[
+              { text: 'good', value: good },
+              { text: 'neutral', value: neutral },
+              { text: 'bad', value: bad },
+              { text: 'average', value: average },
+              { text: 'positive', value: positive },
+            ].map((stat) => (
+              <StatisticLine key={stat.text} text={stat.text} value={stat.value} />
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   )
 }
